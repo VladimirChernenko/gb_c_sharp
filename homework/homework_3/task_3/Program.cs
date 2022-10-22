c20e014831b7f36572db35bed41d76ce0039ef39
Vladimir <chernenkovovan3012@gmail.com>
2022-10-22 21:50:19 +0300

    create homework_3

---
+++ homework/homework_3/task_3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number > 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+void SequenceCube(int num)
+{
+    Console.Write(1);
+    for (int i = 2; i <= num; i++)
+    {
+        Console.Write($", {Math.Pow(i, 3)}");
+    }
+}
+
+int num = Prompt("Enter integer greater then zero: ");
+if (ValidateNumber(num))
+{
+    SequenceCube(num);
+}
+else
+{
+    Console.WriteLine("Input Error.");
+}
