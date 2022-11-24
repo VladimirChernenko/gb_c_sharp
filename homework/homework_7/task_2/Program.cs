b615cea29357ee96f49eb162f532c970e06ee689
Vladimir <chernenkovovan3012@gmail.com>
2022-11-24 19:56:56 +0300

    create homework_8

---
+++ homework/homework_7/task_2/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+double[,] CreateMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = new Random().NextDouble() * new Random().Next(-10, 10);
+            }
+        }
+        return matrix;  
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(matrix[i, j], 2)} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool Validate(double[,] matrix, int position)
+{
+    if (position > 0 && matrix.GetLength(0) * matrix.GetLength(1) >= position) return true;
+    else return false;
+}
+
+double PositionMatrixNumber(double[,] matrix, int position)
+{
+    int positionString = position / matrix.GetLength(1) - 1;
+    int positionColumn = position % matrix.GetLength(1) - 1;
+    if (positionString < 0) positionString = 0;
+    if (positionColumn < 0) positionColumn = 0;
+    return Math.Round(matrix[positionString, positionColumn], 2);
+}
+
+void Main()
+{
+    int stringMatrix = new Random().Next(1, 6);
+    int columnMatrix = new Random().Next(1, 6);
+    int position = EnterNumber("Position number in matrix: ");
+    double[,] matrix = CreateMatrix(stringMatrix, columnMatrix);
+    double resultNumber = PositionMatrixNumber(matrix, position);
+    PrintMatrix(matrix);
+    if (Validate(matrix, position)) Console.Write($"{resultNumber}");
+    else Console.Write("There is no such number in the array");
+}
+
+Main();
---
+++ homework/homework_7/task_3/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new double[m, n];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = new Random().Next(-100, 100);
+            }
+        }
+        return matrix;  
+}
+
+double Average(double array)
+{
+    double result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i];   
+    }
+    return result / Convert.ToDouble(array.Length);
+}
+
+double[] AverageColumn(int[,] matrix)
+{
+    int[] column = new int[matrix.GetLength(1)];
+    int[] result = new int[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            column[j] = matrix[j, i];  
+        }
+        result[i] = Average(column);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write($"[{array[0]}");
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    Console.Write("]");
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(matrix[i, j], 2)} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] matrix = CreateMatrix(4,5);
+int result_array
