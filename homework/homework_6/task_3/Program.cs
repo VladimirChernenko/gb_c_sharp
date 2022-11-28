fddc0fa9aea1a65ccd46e2c9b28dc13abfdabcee
Vladimir <chernenkovovan3012@gmail.com>
2022-11-28 11:59:35 +0300

    add task_1 and task_2 to homework_6

--- homework/homework_6/task_3/Program.cs
+++ homework/homework_6/task_3/Program.cs
@@ -3,3 +3,29 @@
 // 3 4 5
 // 3+5=8, 1+2+3=6, 8-6=2
 
+double[,] CreateMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = new Random().NextDouble() * new Random().Next(-10, 10);
+            }
+        }
+        return matrix;  
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(matrix[i, j], 2)} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] MaxNumber()
